6f92a0a25bb011f535441c711a5d89f0aa002000
id <happytobi@tscoding.de>
2024-05-14 10:52:04 +0200

    Update go version

--- Makefile
+++ Makefile
@@ -7,5 +7,7 @@ all: binary
 
 binary:
 	echo "build terraform provider"
+	GOCACHE=off \
 	cd /workspaces/dev-terraform-provider/src/terraform-provider-azurerm; \
+	rm -rf /workspaces/dev-terraform-provider/build; \
 	$(GO) build -gcflags=all="-N -l" -o /workspaces/dev-terraform-provider/build/terraform/terraform-azurerm/terraform-provider-azurerm
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,9 +3,18 @@
 echo "Login into azure az login ..."
 az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 
-echo "Cloing latest sourcecode from github into docker volume ..."
+echo "Clone latest sourcecode from github into docker volume ..."
 if [ -d "src/terraform-provider-azurerm" ]; then
     echo "terraform already cloned"
 else
     cd src && git clone https://github.com/hashicorp/terraform-provider-azurerm.git
+
+    echo "installing go packages"
+    go install -v  golang.org/x/tools/gopls@latest
+    go install -v  github.com/cweill/gotests/gotests@latest
+    go install -v  github.com/fatih/gomodifytags@latest
+    go install -v  github.com/josharian/impl@latest
+    go install -v  github.com/haya14busa/goplay/cmd/goplay@latest
+    go install -v  github.com/go-delve/delve/cmd/dlv
+    go install -v  honnef.co/go/tools/cmd/staticcheck
 fi
