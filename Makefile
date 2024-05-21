f9ec180d87d7e216fd3b65975f67743e173fcde5
id <happytobi@tscoding.de>
2024-05-21 20:32:07 +0000

    Enable terraformad development

--- Makefile
+++ Makefile
@@ -9,5 +9,12 @@ binary:
 	echo "build terraform provider"
 	GOCACHE=off \
 	cd /workspaces/dev-terraform-provider/src/terraform-provider-azurerm; \
-	rm -rf /workspaces/dev-terraform-provider/build; \
+	rm -rf /workspaces/dev-terraform-provider/build/terraform/terraform-azurerm; \
 	$(GO) build -gcflags=all="-N -l" -o /workspaces/dev-terraform-provider/build/terraform/terraform-azurerm/terraform-provider-azurerm
+
+binary-ad:
+	echo "build terraform provider azuread"
+	GOCACHE=off \
+	cd /workspaces/dev-terraform-provider/src/terraform-provider-azuread; \
+	rm -rf /workspaces/dev-terraform-provider/build/terraform/terraform-azuread; \
+	$(GO) build -gcflags=all="-N -l" -o /workspaces/dev-terraform-provider/build/terraform/terraform-azuread/terraform-provider-azuread
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 echo "Login into azure az login ..."
-az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+az login --service-principal -u "${ARM_CLIENT_ID}" -p "${ARM_CLIENT_SECRET}" --tenant "${ARM_TENANT_ID}"
 
-echo "Clone latest sourcecode from github into docker volume ..."
-if [ -d "src/terraform-provider-azurerm" ]; then
-    echo "terraform already cloned"
-else
-    cd src && git clone https://github.com/hashicorp/terraform-provider-azurerm.git
-
+if ! command -v gofmt &> /dev/null; then
     echo "installing go packages"
     go install -v  golang.org/x/tools/gopls@latest
     go install -v  github.com/cweill/gotests/gotests@latest
@@ -17,4 +12,23 @@ else
     go install -v  github.com/haya14busa/goplay/cmd/goplay@latest
     go install -v  github.com/go-delve/delve/cmd/dlv
     go install -v  honnef.co/go/tools/cmd/staticcheck
+fi
+
+echo "Clone latest terraform provider azurerm from github into docker volume ..."
+if [ -d "/workspaces/dev-terraform-provider/src/terraform-provider-azurerm" ]; then
+    echo "terraform already cloned"
+else
+    pushd "/workspaces/dev-terraform-provider/src" || exit
+        git clone https://github.com/hashicorp/terraform-provider-azurerm.git
+    popd || exit
+
+fi
+
+echo "Clone latest terraform provider azuread from github into docker volume ..."
+if [ -d "/workspaces/dev-terraform-provider/src/terraform-provider-azuread" ]; then
+    echo "terraform already cloned"
+else
+    pushd "/workspaces/dev-terraform-provider/src" || exit
+        git clone https://github.com/hashicorp/terraform-provider-azuread.git
+    popd || exit
 fi
